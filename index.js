b16a6a7b7fae2c16c7d7582e00813a7cadd4b3d4
jonschlinkert <dev@sellside.com>
2014-11-02 03:40:10 -0500

    v0.2.0
      - fixes camelcasing, clean up

--- index.js
+++ index.js
@@ -8,23 +8,64 @@
 'use strict';
 
 var path = require('path');
+var fs = require('fs-utils');
 var mapFiles = require('map-files');
-var merge = require('mixin-object');
-var reader = require('./readers');
+var extend = require('extend-shallow');
 
-
-module.exports = function (patterns, options) {
-  options = merge({name: camelize, read: read}, options);
-  return mapFiles(patterns, options);
+module.exports = function(patterns, options) {
+  return mapFiles(patterns, extend({
+    name: camelize,
+    read: function(fp) {
+      fp = path.resolve(fp);
+      var read = reader(path.extname(fp));
+      return read(fp);
+    }
+  }, options));
 };
+
+/**
+ * This is just a minimal start, pull requests welcome
+ * for adding extensions/readers to the list.
+ *
+ * @param {String} `ext`
+ * @return {Function} The file reader to use for the given `ext`
+ * @api private
+ */
+
+function reader(ext) {
+  switch (ext) {
+    // Requireable files
+    case '.js':
+    case '.json':
+      return require;
+
+    // Strings
+    case '.txt':
+    case '.md':
+    case '.hbs':
+      return fs.readFileSync;
+
+    // Data
+    case '.yml':
+    case '.yaml':
+      return fs.readYAMLSync;
+    }
+}
+
+
+/**
+ * Camelcase rename function to pass to [map-files].
+ *
+ * @param  {String} `fp`
+ * @return {String}
+ */
 
 function camelize(fp) {
   var str = path.basename(fp, path.extname(fp));
-  return str.replace(/-(.)/, function (_, s) {
-    return s.toUpperCase();
+  if (str.length === 1) {
+    return str;
+  }
+  return str.toLowerCase().replace(/[-_.](\w|$)/g, function (_, ch) {
+    return ch.toUpperCase();
   });
-}
-
-function read(fp) {
-  return reader(path.extname(fp))(path.resolve(fp));
 }
